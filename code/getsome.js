11e8b40c7aa972a574640ba652eee6dde2efd00a
aarobc <aaron.b.christiansen@gmail.com>
2015-07-05 21:46:11 -0600

    added database stuff

--- code/getsome.js
+++ code/getsome.js
@@ -1,5 +1,3 @@
-
-
 var url = 'a.4cdn.org';
 var path = '/b/catalog.json'
 var board = 'b';
@@ -7,53 +5,62 @@ var board = 'b';
 var request = require('request');
 var fs = require('fs');
 var async = require('async');
-// var path = require('path');
 
-function getImg(post, cb){
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('/data/db.sqlite');
+
+
+function getImg(post, board, cb){
     // console.log(img);
     var addr = "http://i.4cdn.org/" + board + "/" + post.tim + post.ext; 
     console.log(addr);
     // request(addr).pipe(fs.createWriteStream('/data/images/' + post.filename + post.ext));
 
-    var picStream = fs.createWriteStream('/data/images/' + post.filename + post.ext);
+    var picStream = fs.createWriteStream('/data/' + board + '/' + post.filename + post.ext);
+
+    // setting up the pic stream with callbacks
     picStream.on('close', function() {
         console.log('file done');
         cb(null, post);
     });
+
+    // run it
     request(addr).pipe(picStream); 
 
 }
 
+// requests and returns array object of all 
 function allThreads(board, callback){
+
     request('http://a.4cdn.org/' + board + '/threads.json', function(error, response, body){
         var parsed = JSON.parse(body);
         var threads = Array();
-        // for(a in parsed){
-        //     threads = threads.concat(parsed[a].threads);
-        // }
-        threads = threads.concat(parsed[0].threads);
+        for(a in parsed){
+            threads = threads.concat(parsed[a].threads);
+        }
+        // console.log("tim what");
+        // console.log(threads);
         callback(threads);
     });
 
 }
 
-function posts(board, thread, callback){
-    request('http://a.4cdn.org/' + board + '/thread/' + thread.no +'.json', function(error, response, body){
+function thread(thread, board, callback){
+    // request('http://a.4cdn.org/' + board + '/thread/' + thread.no +'.json', function(error, response, body){
+    request('http://a.4cdn.org/' + board + '/thread/' + thread +'.json', function(error, response, body){
+        if(!body){return;}
         var parsed = JSON.parse(body);
         var posts = parsed.posts;
         iposts = Array();
         for(a in posts){
-            // console.log(posts[a]);
             // should do some analasys here
 
             // if the post has an image
             if(posts[a].tim){
-                // getImg(posts[a]);
                 iposts = iposts.concat(posts[a]);
-                // var uurl = posts[a].tim + posts[a].ext;
-                // console.log(iposts.length);
             }
         }
+
         // workaround to get it to run after the for loop is completed
         setTimeout(function(){
             callback(iposts);
@@ -62,27 +69,88 @@ function posts(board, thread, callback){
 
 }
 
-allThreads('b', function(tlist){
+// allThreads('b', function(tlist){
+//
+//     return;
+//     // go thourgh all the threads in post and download the image
+//     for(i in tlist){
+//     // var i = 0;
+//         thread('b', tlist[i], function(posts){
+//                 console.log(posts.length);
+//                 async.eachSeries(posts, function iterator(post, callback) {
+//                     console.log(post.no);
+//                     getImg(post, board, callback);
+//                 });
+//         });
+//     }
+//
+// });
 
-    // go thourgh all the threads in post and download the image
-    for(i in tlist){
-    // var i = 0;
-        posts('b', tlist[i], function(posts){
-            // for(j in posts){
-                // var post = posts[j];
-                // console.log("num posts:");
-                console.log(posts.length);
-                // console.log(post.no);
-                // getImg(post);
-                async.eachSeries(posts, function iterator(post, callback) {
-                    console.log(post.no);
-                    getImg(post, callback);
-                });
+function setupDB(){
+    db.serialize(function() {
+      db.run("DROP TABLE threads");
+      db.run("CREATE TABLE IF NOT EXISTS `threads` (\
+                `id` INT NOT NULL,\
+                `board` text NOT NULL,\
+                `last_modified` VARCHAR(45) NULL, \
+                `ignored` INT NULL DEFAULT 0,\
+                `added` VARCHAR(45) NOT NULL,\
+                PRIMARY KEY (`id`))");
+      db.run("DROP TABLE posts");
 
-            // }
+      db.run("\
+             CREATE TABLE IF NOT EXISTS `posts` (\
+      `id` INT NOT NULL,\
+      `img_path` VARCHAR(45) NULL,\
+      `text` TEXT NULL,\
+      `threads_id` INT NOT NULL,\
+      `created` TIMESTAMP(45) NULL,\
+      PRIMARY KEY (`id`, `threads_id`))");
 
-        });
-    }
+      // db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
+      //     console.log(row.id + ": " + row.info);
+      // });
+    });
 
-});
+    populateThreads();
+    populatePosts();
+    // db.close();
+}
 
+
+// load all thread ids into db, all based on UTC time
+function populateThreads(){
+
+    allThreads(board, function(tlist){
+        // console.log(tlist);
+        var stmt = db.prepare("INSERT INTO threads VALUES (?, ?, datetime(?, 'unixepoch', 'utc'), 0, datetime('now', 'utc'))");
+        for(i in tlist){
+            stmt.run(tlist[i].no, 'b', tlist[i].last_modified);
+        }
+        stmt.finalize();
+    });
+}
+
+
+function populatePosts(board){
+      db.each("SELECT * from threads", function(err, row) {
+          // console.log(row.id);
+          thread(row.id, row.board, function(thr){
+              console.log(thr);
+              // TODO: get this working
+              db.run("INSERT INTO posts (id, text, threads_id, created) VALUES  (" +
+                 thr.no + ", '"+ thr.com + "', " + row.id +"," + thr.time + ")");
+
+          });
+          // console.log(row);
+          // return;
+          // console.log(row.id + ": " + row.last_modified);
+        // var stmt = db.run("INSERT INTO posts VALUES (, ?, datetime(?, 'unixepoch', 'utc'), 0, datetime('now', 'utc'))");
+        //   console.log(row);
+      });
+    
+}
+
+// setupDB();
+populatePosts();
+// testa();
